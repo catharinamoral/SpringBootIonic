76e6d53ca11d1ab428ad5c537c2aa0e780dd5d34
C. Moral Hermacula <c.moral.hermacula@avanade.com>
2020-06-16 14:19:09 -0300

    Criar operação de instanciacao

--- src/main/java/com/cathariamoral/cursosb/CursosbApplication.java
+++ src/main/java/com/cathariamoral/cursosb/CursosbApplication.java
@@ -1,13 +1,33 @@
 package com.cathariamoral.cursosb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cathariamoral.cursosb.domain.Categoria;
+import com.cathariamoral.cursosb.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursosbApplication {
+public class CursosbApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursosbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
